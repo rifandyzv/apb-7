24859b373477bd01606505ec6fbf179a531c55c4
rifandyzv <rifandy2@gmail.com>
2021-09-25 14:17:11 +0700

    add assets & update app.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         </div>
 
         <img
-          src={`${process.env.PUBLIC_URL}/atas.png`}
+          src={`${process.env.PUBLIC_URL}/assets/atas_polos.png`}
           alt="Background section 1"
         />
       </section>
@@ -21,8 +21,55 @@ function App() {
           <p>asdw</p>
         </div>
         <img
-          src={`${process.env.PUBLIC_URL}/awan.png`}
+          src={`${process.env.PUBLIC_URL}/assets/awan.png`}
           alt="Background Section 2"
+        />
+      </section>
+      <section>
+        <div className="content">{/* content disini */}</div>
+        <img
+          src={`${process.env.PUBLIC_URL}/assets/day1-2.png`}
+          alt="Background Section 3"
+        />
+      </section>
+      <section>
+        <div className="content">{/* content disini */}</div>
+        <img
+          src={`${process.env.PUBLIC_URL}/assets/day3-4.png`}
+          alt="Background Section 4"
+        />
+      </section>
+      <section>
+        <div className="content">
+          {/* <h1>asdasd</h1>
+          <p>asdw</p>
+          content disini */}
+        </div>
+        <img
+          src={`${process.env.PUBLIC_URL}/assets/day5-6.png`}
+          alt="Background Section 5"
+        />
+      </section>
+      <section>
+        <div className="content">
+          {/* <h1>asdasd</h1>
+          <p>asdw</p>
+          content disini */}
+        </div>
+        <img
+          src={`${process.env.PUBLIC_URL}/assets/day7-8.png`}
+          alt="Background Section 6"
+        />
+      </section>
+      <section>
+        <div className="content">
+          {/* <h1>asdasd</h1>
+          <p>asdw</p>
+          content disini */}
+        </div>
+        <img
+          src={`${process.env.PUBLIC_URL}/assets/day-9.png`}
+          alt="Background Section 7"
         />
       </section>
     </div>
