1b3a550b69c44c7cab59545938f917d1c41bf473
mhmmdjafarg <mjafarashadiq712@gmail.com>
2021-09-26 01:10:49 +0700

    add day 3 to 6

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import './App.css'
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      {/* Home */}
       <section>
         <div className="content">
           <h1>Tes</h1>
           <p>Haii</p>
         </div>
-
         <img
           src={`${process.env.PUBLIC_URL}/assets/atas_polos.png`}
           alt="Background section 1"
         />
       </section>
 
+      {/* Apa itu APB */}
       <section>
         <div className="content">
           <h1>asdasd</h1>
@@ -25,6 +26,8 @@ function App() {
           alt="Background Section 2"
         />
       </section>
+
+      {/* Day 1 - Day 2 */}
       <section>
         <div className="content">{/* content disini */}</div>
         <img
@@ -32,24 +35,79 @@ function App() {
           alt="Background Section 3"
         />
       </section>
-      <section>
-        <div className="content">{/* content disini */}</div>
+
+      {/* Day 3 - Day 4 */}
+      <section id="section4">
         <img
           src={`${process.env.PUBLIC_URL}/assets/day3-4.png`}
           alt="Background Section 4"
         />
-      </section>
-      <section>
-        <div className="content">
-          {/* <h1>asdasd</h1>
-          <p>asdw</p>
-          content disini */}
+
+        <div className="text-box" id="day3">
+          <span>Pemrograman dasar [2] :</span>
+          <br />
+          <span>1. Perulangan</span>
+          <br />
+          <span>2. Array</span>
+          <br />
+          <span>3. Subprogram (fungsi dan prosedur)</span>
         </div>
+
+        <h2 className="day-counter" id="day-counter-3">
+          Day 3
+        </h2>
+
+        <div className="text-box" id="day4">
+          <span>Fun Day :</span>
+          <br />
+          <span>1. Kriptografi + game</span>
+          <br />
+          <span>2. Keamanan Siber</span>
+          <br />
+        </div>
+
+        <h2 className="day-counter" id="day-counter-4">
+          Day 4
+        </h2>
+      </section>
+
+      {/* Day 5 - Day 6 */}
+      <section id="section5">
         <img
           src={`${process.env.PUBLIC_URL}/assets/day5-6.png`}
           alt="Background Section 5"
         />
+
+        <div className="text-box" id="day5">
+          <span>1. Web Development [1] : HTML</span>
+          <br />
+          <span>2. UI/UX</span>
+          <br />
+          <span>3. Web Development [2] :</span>
+          <br />
+          <span>- CSS</span>
+          <br />
+          <span>- Javascript</span>
+          <br />
+        </div>
+
+        <h2 className="day-counter" id="day-counter-5">
+          Day 5
+        </h2>
+
+        <div className="text-box" id="day6">
+          <span>1. Game development</span>
+          <br />
+          <span>2. AI</span>
+          <br />
+        </div>
+
+        <h2 className="day-counter" id="day-counter-6">
+          Day 6
+        </h2>
       </section>
+
+      {/* Day 7 - Day 8 */}
       <section>
         <div className="content">
           {/* <h1>asdasd</h1>
@@ -61,6 +119,8 @@ function App() {
           alt="Background Section 6"
         />
       </section>
+
+      {/* Day 9 - bawah*/}
       <section>
         <div className="content">
           {/* <h1>asdasd</h1>
@@ -73,7 +133,7 @@ function App() {
         />
       </section>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
