091741afde2ed9084264ef35487d43c0eaa15239
rifandyzv <rifandy2@gmail.com>
2021-09-26 12:53:10 +0700

    add section2-3

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+import './App.css'
 
 function App() {
   return (
@@ -16,10 +16,19 @@ function App() {
       </section>
 
       {/* Apa itu APB */}
-      <section>
-        <div className="content">
-          <h1>asdasd</h1>
-          <p>asdw</p>
+      <section id="section2">
+        <div className="apb-desc">
+          <h1 className="section2-header">Apa itu APB?</h1>
+          <p>
+            Program Aku Pasti Bisa/APB 7.0 adalah program edukasi yang berbasis
+            pengajaran dan diadakan oleh Pengabdian Masyarakat HMIF ITB. Program
+            ini merupakan program lanjutan dari "Aku Pasti Bisa 6.0" pada tahun
+            2020.
+            <br />
+            <br />
+            Tema yang diangkat pada aku pasti bisa tahun ini membawa tema
+            pengenalan keinformatikaan kepada masyarakat umum.
+          </p>
         </div>
         <img
           src={`${process.env.PUBLIC_URL}/assets/awan.png`}
@@ -28,8 +37,37 @@ function App() {
       </section>
 
       {/* Day 1 - Day 2 */}
-      <section>
-        <div className="content">{/* content disini */}</div>
+      <section id="section3">
+        <h1 className="timeline">Timeline</h1>
+
+        <div className="text-box" id="day1">
+          <span>1. Overview Kegiatan</span>
+          <br />
+          <span>2. Intro to informatics</span>
+          <br />
+          <span>3. Keinformatikaan dalam kehidupan sehari-hari</span>
+          <br />
+          <span>4. Computational thinking</span>
+        </div>
+
+        <h2 className="day-counter" id="day-counter-1">
+          Day 1
+        </h2>
+
+        <div className="text-box" id="day2">
+          <span>Pemrograman dasar (1) :</span>
+          <br />
+          <span>1. Intro to logic</span>
+          <br />
+          <span>2. Input-output</span>
+          <br />
+          <span>3. Percabangan</span>
+        </div>
+
+        <h2 className="day-counter" id="day-counter-2">
+          Day 2
+        </h2>
+
         <img
           src={`${process.env.PUBLIC_URL}/assets/day1-2.png`}
           alt="Background Section 3"
@@ -133,7 +171,7 @@ function App() {
         />
       </section>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
